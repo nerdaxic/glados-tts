9d3a606f4a47447cfdccbbdfa84b06cac394e7f7
jhughesbiot <jonathan.hughes@buildingsiot.com>
2022-04-01 09:34:44 -0700

    Add optional audio cach path option Update audio mounting option Stop existing containers before starting a new one

--- start.sh
+++ start.sh
@@ -20,6 +20,7 @@ BUILD=false
 BUILD_CACHE=true
 CACHE=false
 DAEMON=false
+AUDIO_PATH=""
 
 #Check all arguments to assign build values
 for i in $@; do
@@ -44,6 +45,18 @@ for i in $@; do
 	esac
 done
 
+#Allow an optional audio cache path to be provided
+while getopts ":c:" opt; do
+	case $opt in
+		c)
+			if [ ! -z "$OPTARG" ];then
+				AUDIO_PATH="$OPTARG"
+			fi
+			;;
+
+	esac
+done
+
 #If image does not already exists, override the BUILD option
 if [[ -z "$(docker images -q $IMAGE_NAME)" ]];then
 	echo "Image does not exist. Setting build option to true"
@@ -65,15 +78,22 @@ if [ "$DAEMON" = true ]; then
 	DAEMON_ARG="-d"
 fi
 
-CACHE_ARG=""
-if [ "$CACHE" = true ]; then
+if [[ "$CACHE" = true && -z "$AUDIO_PATH" ]]; then
 	#ToDo: Figure out how to make this method handle spaces
 	#in the path.  Currently breaks if there is a space
 	#in the current directory path.
-	CACHE_ARG="-v "${PWD}"/audio:/glados-tts/audio"
+	AUDIO_PATH="${PWD}"/audio
 fi
 
-RUN_ARGS="$DAEMON_ARG $CACHE_ARG"
+# Create the mount point command based on the audio path
+# provided.  Currently breaks if there is a space
+# in the directory path.
+AUDIO_MOUNT=""
+if [ ! -z "$AUDIO_PATH" ]; then
+	AUDIO_MOUNT="--mount type=bind,source="$AUDIO_PATH",target=/glados-tts/audio"
+fi
+
+RUN_ARGS="$DAEMON_ARG $AUDIO_MOUNT"
 
 #Create a folder to cache all the audio files locally
 
@@ -84,7 +104,14 @@ echo -e "docker run --name $IMAGE_NAME
 		   $RUN_ARGS
 		   $IMAGE_NAME python3 engine.py"
 
+#Stop any existing instance of this container.
+#No need to check if it's already running,
+#just call this every time
+echo "Stopping existing containers"
+docker stop $IMAGE_NAME
+
 #Start the image, the tts engine and map the ports
+echo "Starting glados-tts"
 docker run --name $IMAGE_NAME \
 		   --rm \
 		   -p 8124:8124 \
