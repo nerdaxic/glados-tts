7eb7ce90fa9c0d57cd413719b49a0e425fe79fb0
sneer <andresduranrod@gmail.com>
2022-06-20 19:09:18 -0600

    Creating Glados class to enclose the logic of the tts

--- glados.py
+++ glados.py
@@ -16,122 +16,116 @@ logging.basicConfig(filename='glados_service.log',
     datefmt='%Y-%m-%d %H:%M:%S',level=logging.DEBUG)
 
 #Global variables
-glados = None
-vocoder = None
-device = None
-audioPath = "audio/"
+audio_path = "audio/"
 
-def printedLog(message):
+class Glados:
+    glados_model = None
+    vocoder = None
+    device = None
+
+    def __init__(self):
+        pass
+
+    def get_available_device(self,option_devices):
+        if "vulkan" in option_devices and torch.is_vulkan_available():
+            device_found = 'vulkan'
+        elif "cuda" in option_devices and torch.cuda.is_available():
+            device_found = 'cuda'
+        else:
+            device_found = 'cpu'
+        printed_log(f"Device selected: {device_found}.")
+        return device_found
+
+    def load_models(self):
+        self.glados_model = torch.jit.load('models/glados.pt')
+        self.vocoder = torch.jit.load('models/vocoder-gpu.pt', map_location=self.device)
+
+        for i in range(4):
+            init = self.glados_model.generate_jit(prepare_text(str(i)))
+            init_mel = init['mel_post'].to(self.device)
+            init_vo = self.vocoder(init_mel)
+        printed_log(f"Models loaded.")
+
+    def load_glados_model(self):
+        #option_devices = ["vulkan","cuda"]
+        option_devices = []
+        while(self.glados_model==None):
+            self.device = self.get_available_device(option_devices)
+            try:
+                self.load_models()
+            except:
+                printed_log(f"Execption loading device "+self.device)
+                if(self.device != 'cpu'): option_devices.remove(self.device)
+                else: sys.exit()
+
+    def get_audio_from_text(self,text):
+    	# Tokenize, clean and phonemize input text
+        phonemized_text = prepare_text(text).to(self.device)
+        with torch.no_grad():
+            # Generate generic TTS-output
+            old_time = time.time()
+            tts_output = self.glados_model.generate_jit(phonemized_text)
+
+            # Use HiFiGAN as vocoder to make output sound like GLaDOS
+            mel = tts_output['mel_post'].to(self.device)
+            audio = self.vocoder(mel)
+            print_timelapse("The audio sample",old_time)
+
+            # Normalize audio to fit in wav-file
+            audio = audio.squeeze()
+            audio = audio * 32768.0
+            audio = audio.cpu().numpy().astype('int16')
+        return audio
+
+    def generate_tts(self, input_text):
+        output_key = input_text.replace(" ", "_")
+        audio_file_exist = check_audio_file(output_key)
+        if (audio_file_exist):
+            output_file = f"{audio_path}{output_key}.wav"
+        else:
+            audio = self.get_audio_from_text(input_text)
+            output_file = save_audio_file(audio,output_key)
+        play_sound(output_file)
+
+def printed_log(message):
     logging.info(message)
     print(message)
 
-def printTimelapse(processName,old_time):
-    printedLog(f"{processName} took {str((time.time() - old_time) * 1000)} ms")
+def print_timelapse(processName,old_time):
+    printed_log(f"{processName} took {str((time.time() - old_time) * 1000)} ms")
 
-def selectDevice(optionDevices):
-    if "vulkan" in optionDevices and torch.is_vulkan_available():
-        device = 'vulkan'
-    elif "cuda" in optionDevices and torch.cuda.is_available():
-        device = 'cuda'
-    else:
-        device = 'cpu'
-    printedLog(f"Device selected: {device}.")
-    return device
-
-def loadModelsOnDevice(device):
-    glados = torch.jit.load('models/glados.pt')
-    vocoder = torch.jit.load('models/vocoder-gpu.pt', map_location=device)
-
-    # Prepare models in RAM
-    for i in range(4):
-        init = glados.generate_jit(prepare_text(str(i)))
-        init_mel = init['mel_post'].to(device)
-        init_vo = vocoder(init_mel)
-    printedLog(f"Models loaded.")
-    return glados,vocoder
-
-def loadModels():
-    global glados,vocoder,device
-    optionDevices = ["vulkan","cuda"]
-    while(glados==None):
-        # Select the device
-        deviceAux = selectDevice(optionDevices)
-        try:
-            # Load models
-            glados,vocoder = loadModelsOnDevice(deviceAux)
-            device = deviceAux
-        except:
-            printedLog(f"Execption loading device "+deviceAux)
-            optionDevices.remove(deviceAux)
-    return glados!=None
-
-def playSound(fileName):
+def play_sound(fileName):
     if 'winsound' in mod:
         winsound.PlaySound(fileName, winsound.SND_FILENAME)
     else:
         call(["aplay", f"./{fileName}"])
 
-def saveAudioFile(audio,output_key=None):
+def save_audio_file(audio,output_key=None):
     output_file_name = "GLaDOS-tts-tempfile"
     if(output_key and len(output_key)<200):
         output_file_name = output_key
-    output_file = (f"{audioPath}{output_file_name}.wav")
+    output_file = (f"{audio_path}{output_file_name}.wav")
     # Write audio file to disk at 22,05 kHz sample rate
     logging.info(f"Saving audio as {output_file}")
     write(output_file, 22050, audio)
     return output_file
 
-def checkAudioFile(file_name):
-    alreadyExist = os.path.exists(f"{audioPath}{file_name}.wav")
+def check_audio_file(file_name):
+    alreadyExist = os.path.exists(f"{audio_path}{file_name}.wav")
     return alreadyExist
 
-def glados_tts(text):
-	# Tokenize, clean and phonemize input text
-    x = prepare_text(text).to(device)
-    with torch.no_grad():
-        # Generate generic TTS-output
-        old_time = time.time()
-        tts_output = glados.generate_jit(x)
-
-        # Use HiFiGAN as vocoder to make output sound like GLaDOS
-        mel = tts_output['mel_post'].to(device)
-        audio = vocoder(mel)
-        printTimelapse("The audio sample",old_time)
-
-        # Normalize audio to fit in wav-file
-        audio = audio.squeeze()
-        audio = audio * 32768.0
-        audio = audio.cpu().numpy().astype('int16')
-    return audio
-
-def executeFromTerminal(input_text):
-    output_key = input_text.replace(" ", "_")
-    audioFileExist = checkAudioFile(output_key)
-    if (audioFileExist):
-        output_file = f"{audioPath}{output_key}.wav"
-    else:
-        audio = glados_tts(input_text)
-        output_file = saveAudioFile(audio,output_key)
-    playSound(output_file)
-
 def main():
-    printedLog("Initializing TTS Engine...")
-    loadModels()
+    printed_log("Initializing TTS Engine...")
+    glados = Glados()
+    glados.load_glados_model()
     if(len(sys.argv)==2):
-        printedLog("Using command line argument as text")
-        executeFromTerminal(sys.argv[1])
+        printed_log("Using command line argument as text")
+        glados.generate_tts(sys.argv[1])
     else:
         while(1):
-            printedLog("Using user input as text")
+            printed_log("Using user input as text")
             input_text = input("Input: ")
-            output_key = input_text.replace(" ", "_")
-            audioFileExist = checkAudioFile(output_key)
-            if (audioFileExist):
-                output_file = f"{audioPath}{output_key}.wav"
-            else:
-                audio = glados_tts(input_text)
-                output_file = saveAudioFile(audio,output_key)
-            playSound(output_file)
+            glados.generate_tts(input_text)
 
 if __name__ == "__main__":
     main()
